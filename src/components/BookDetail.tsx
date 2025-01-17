1f37da346dad116b5283272c66daa1a0ab7afa6f
cuneytakcay <cuneytakcay@gmail.com>
2025-01-16 21:56:40 -0700

    Updating Book Detail component

--- src/components/BookDetail.tsx
+++ src/components/BookDetail.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import styles from './BookDetail.module.css';
+import Loader from './Loader';
 import { Book } from '../types/Book';
+import styles from './BookDetail.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const BookDetail: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -20,11 +22,9 @@ const BookDetail: React.FC = () => {
         setBook(response.data);
 
         // Set timeout for loading effect after fetching data
-        const timer = setTimeout(() => {
+        setTimeout(() => {
           setLoading(false);
         }, 1000);
-
-        return () => clearTimeout(timer);
       } catch (error) {
         console.error('Error fetching book details:', error);
         setLoading(false); // Ensure loading is set to false on error
@@ -34,30 +34,61 @@ const BookDetail: React.FC = () => {
     fetchBookDetails();
   }, [id]);
 
-  if (loading) return <div className={styles.skeletonLoader}></div>;
-
-  if (!book) return <div>No book details available.</div>;
-
   const handleClick = () => {
     navigate('/');
   };
 
-  const placeholderImage =
-    'https://placehold.co/400x600/e2e8f0/1e293b?text=No+Cover+Available';
-
   return (
     <div>
-      {book && (
+      {loading ? (
+        <Loader />
+      ) : book ? (
         <div className={styles.bookDetail}>
           <button onClick={handleClick}>Go to Book Search</button>
           <h1>{book.volumeInfo.title}</h1>
-          <p>Authors: {book.volumeInfo.authors?.join(', ')}</p>
-          <img
-            src={book.volumeInfo.imageLinks?.thumbnail || placeholderImage}
-            alt={book.volumeInfo.title}
-          />
-          <p>{book.volumeInfo.description}</p>
+          <h2>{book.volumeInfo.subtitle}</h2>
+          <p className={styles.authors}>
+            <span style={{ fontWeight: 'bold' }}>Authors:</span>{' '}
+            {book.volumeInfo.authors?.join(', ')}
+          </p>
+          <div className={styles.content}>
+            <img
+              src={
+                book.volumeInfo.imageLinks?.thumbnail ||
+                'https://placehold.co/128x193/e2e8f0/1e293b?text=No+Cover+Available'
+              }
+              alt={
+                book.volumeInfo.imageLinks?.thumbnail
+                  ? `Cover of ${book.volumeInfo.title}`
+                  : 'No cover available'
+              }
+              width={128}
+              height={193}
+            />
+            <div className={styles.info}>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html:
+                    book.volumeInfo.description || 'No description available',
+                }}
+              />
+              <p>
+                <span style={{ fontWeight: 'bold' }}>Categories:</span>{' '}
+                {book.volumeInfo.categories?.join(', ')}
+              </p>
+              <a
+                href={book.volumeInfo.infoLink}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Check it on Google Books
+                <FontAwesomeIcon icon={faArrowRight} />
+              </a>
+            </div>
+          </div>
         </div>
+      ) : (
+        <p style={{ textAlign: 'center' }}>This book cannot be found.</p>
       )}
     </div>
   );
