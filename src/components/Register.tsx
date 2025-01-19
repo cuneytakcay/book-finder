65f247f6d779d476ca8b65c7d1ff24c3527682b1
cuneytakcay <cuneytakcay@gmail.com>
2025-01-18 22:03:58 -0700

    Adding fields to register form

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -4,11 +4,23 @@ const Register: React.FC = () => {
   return (
     <form className={styles.form}>
       <label>
+        First Name
+        <input type='text' required />
+      </label>
+      <label>
+        Last Name
+        <input type='text' required />
+      </label>
+      <label>
         Email
         <input type='email' required />
       </label>
       <label>
         Password
+        <input type='password' required />
+      </label>
+      <label>
+        Confirm Password
         <input type='password' required />
       </label>
       <button type='submit'>Register</button>
