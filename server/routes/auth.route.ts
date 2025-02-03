7ba6b1b6e899d7480bfa438f1beb03bc52f5b5d4
cuneytakcay <cuneytakcay@gmail.com>
2025-02-02 21:44:31 -0700

    Removing express-validator

--- server/routes/auth.route.ts
+++ server/routes/auth.route.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import validator from 'express-validator';
 import {
   registerUser,
   loginUser,
@@ -8,29 +7,15 @@ import {
 import verifyToken from '../middleware/verifyToken';
 
 const router = express.Router();
-const { check } = validator;
 
 // Register to create a user account
-router.post(
-  '/user/register',
-  [
-    check(['firstName', 'lastName'], 'Field cannot be empty').trim().notEmpty(),
-    check('email', 'Not a valid email').isEmail().normalizeEmail(),
-    check('password', 'Not a valid password').matches(
-      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/
-    ),
-    check('confirm', 'Passwords do not match').custom(
-      (value, { req }) => value === req.body.password
-    ),
-  ],
-  async (req: Request, res: Response) => {
-    try {
-      await registerUser(req, res);
-    } catch (error) {
-      res.status(500).json(error);
-    }
+router.post('/user/register', async (req: Request, res: Response) => {
+  try {
+    await registerUser(req, res);
+  } catch (error) {
+    res.status(500).json(error);
   }
-);
+});
 
 // Login to get a JWT token
 router.post('/user/login', async (req: Request, res: Response) => {
