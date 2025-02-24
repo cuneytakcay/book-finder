15556d5b8f58231332eec0320281a1e84437aa58
cuneytakcay <cuneytakcay@gmail.com>
2025-02-23 17:37:52 -0700

    Configuring cors actions

--- api/index.ts
+++ api/index.ts
@@ -18,7 +18,13 @@ const MONGO_URL = process.env.MONGO_URL;
 
 // Middleware for JSON parser and cors
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://book-finder-xu3a.onrender.com/'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  })
+);
 
 // Set up route paths
 app.use('/api/auth', authRoutes);
@@ -26,13 +32,13 @@ app.use('/api/books', booksRoutes);
 
 // Connect to MongoDB and Start the server
 mongoose
-	.connect(MONGO_URL as string)
-	.then(() => {
-		app.listen(PORT, () => {
-			console.log(`Server is running on port ${PORT}`);
-		});
-	})
-	.catch(error => {
-		console.error('Error connecting to MongoDB:', error);
-		process.exit(1);
-	});
+  .connect(MONGO_URL as string)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  });
